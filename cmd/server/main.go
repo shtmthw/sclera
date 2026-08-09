0df9bb6181d60fd3ca928577ab5af06205738f81
Matthew <mathiwasbaroi@gmail.com>
2026-08-09 21:18:56 +0600

    Added pgxpool connection, and implemented in cmd/server/main.go

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/mattthew/sclera/internal/db"
+)
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, relying on real environment variables")
+	}
+
+	ctx := context.Background()
+
+	pool, err := db.ConnectDatabase(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer pool.Close()
+
 	fmt.Println("Sclera server starting...")
 }
---
+++ internal/db/dbCon.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"context"
+	"errors"
+	"log"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func ConnectDatabase(ctx context.Context) (*pgxpool.Pool, error) {
+	connString := os.Getenv("DATABASE_URL")
+	if connString == "" {
+		return nil, errors.New("DATABASE_URL is not set")
+	}
+	pool, err := pgxpool.New(context.Background(), connString)
+
+	if err != nil {
+		log.Println("Failed connecting of pool")
+		return nil, err
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+
+		log.Println("Database is inactive")
+		return nil, err
+	}
+
+	log.Println("Database is active")
+	return pool, nil
+}
