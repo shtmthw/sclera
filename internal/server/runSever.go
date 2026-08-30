0cab4cc1da6abab9721c74888bb9327a0ab7f924
Matthew <mathiwasbaroi@gmail.com>
2026-08-30 08:49:27 +0600

    added redis OTP authenticaiton

--- internal/server/runSever.go
+++ internal/server/runSever.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/redis/go-redis/v9"
+
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/mattthew/sclera/internal/routes"
 )
@@ -21,12 +23,12 @@ func newServer(mux http.Handler) *http.Server {
 	return pointerHttpServer
 }
 
-func RunServer(pool *pgxpool.Pool) {
+func RunServer(pool *pgxpool.Pool, redisClient *redis.Client) {
 	//creates the router
 	mux := http.NewServeMux()
 
 	//assigns the mux router/ServeMux ( has .HandleFunc within it ) to connect the endpoints to the handlerfuncs
-	routes.RegisterUserRoutes(mux, pool)
+	routes.RegisterUserRoutes(mux, pool, redisClient)
 
 	//creates the server and hosts the mux handler to the provided port.
 	server := newServer(mux)
